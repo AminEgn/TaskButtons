fab7df6d2da24d23619f35e89878f5dd9ca04e55
aminx15 <aminx15am@yahoo.com>
2021-10-16 15:55:17 +0330

    Fix single instance running

--- TaskButtons.py
+++ TaskButtons.py
@@ -9,17 +9,30 @@ from PyQt5.QtWidgets import QApplication
 
 
 def single_instance(app_name):
-    apps = os.popen(f'TASKLIST /FI "IMAGENAME eq {app_name.lower()}*"').read()
-    if app_name in apps:
-        return False
-    else:
-        return True
+    """check only one instance of application"""
+    try:
+        # get process list
+        apps = os.popen(f'TASKLIST /FI "IMAGENAME eq {app_name.lower()}*"').read()\
+            .replace('               ', '\n').split('\n')
+        # empty list
+        expected_list = list()
+        for p in apps:
+            if p.lower() == f'{app_name.lower()}.exe':
+                expected_list.append(p)
+        # check number of instances
+        if len(expected_list) > 1:
+            return False
+        else:
+            return True
+    except Exception as e:
+        print(str(e))
 
 
 def main():
     app = QApplication(sys.argv)
     window = MainWindow()
     window.show()
+    # weak reference
     controller = Controller(window)
     sys.exit(app.exec())
 
@@ -29,4 +42,3 @@ if __name__ == '__main__':
         main()
     else:
         sys.exit(-1)
-
