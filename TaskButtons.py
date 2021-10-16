a429ccceecd04579d227ac1cc7804591df13f227
aminx15 <aminx15am@yahoo.com>
2021-10-16 11:56:16 +0330

    Add single instance running for application

--- TaskButtons.py
+++ TaskButtons.py
@@ -2,9 +2,18 @@
 from src import Controller
 from src.ui import MainWindow
 # standard
+import os
 import sys
 # pyqt
 from PyQt5.QtWidgets import QApplication
+
+
+def single_instance(app_name):
+    apps = os.popen(f'TASKLIST /FI "IMAGENAME eq {app_name.lower()}*"').read()
+    if app_name in apps:
+        return False
+    else:
+        return True
 
 
 def main():
@@ -16,4 +25,8 @@ def main():
 
 
 if __name__ == '__main__':
-    main()
+    if single_instance('TaskButton'):
+        main()
+    else:
+        sys.exit(-1)
+
